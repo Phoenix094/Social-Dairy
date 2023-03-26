0e6d9d0425729cced24f49bbbdcee0ba9a6a9bbb
Somesh Chavhan <someshchavhan904@gmail.com>
2023-03-26 20:46:40 +0530

    connection string deleted

--- server/index.js
+++ server/index.js
@@ -14,8 +14,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json());
 
-const URL =
-	"mongodb+srv://phoenix:phoenix94@cluster0.zbsuiom.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, (error) => {
