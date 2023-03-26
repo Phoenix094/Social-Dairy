78a788181581e1c7c5b6edc53f07d2619cf563fe
Somesh Chavhan <someshchavhan904@gmail.com>
2023-03-26 15:36:39 +0530

    All the single Post are created

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import Post from "./Post/Post";
 import useStyles from "./style";
 
 const Posts = () => {
-	const classes = useStyles();
-	const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
 
-	console.log(posts);
+  console.log(posts);
 
-	return (
-		<>
-			<h1>Posts</h1>
-			<Post />
-			<Post />
-		</>
-	);
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      spacing={3}
+      alignItems="strech"
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Posts;
