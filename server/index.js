9ab471e387fb4009ba1780e3735dc3976cb9abf8
Somesh Chavhan <someshchavhan904@gmail.com>
2023-03-26 20:45:43 +0530

    Added dotenv file move variable

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,9 +16,9 @@ app.use(express.json());
 
 const URL =
 	"mongodb+srv://phoenix:phoenix94@cluster0.zbsuiom.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(URL, (error) => {
+mongoose.connect(process.env.CONNECTION_URL, (error) => {
 	if (error) {
 		console.log("Not connected to mongo");
 	} else {
