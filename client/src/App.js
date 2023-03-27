888c13d0897d4fadc29912135fa63434ece7b605
Somesh Chavhan <someshchavhan904@gmail.com>
2023-03-27 15:38:05 +0530

    Make navbar static

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
 	return (
 		<>
 			<Container maxWidth="lg">
-				<AppBar className={classes.appBar} position="sticky" color="inherit">
+				<AppBar className={classes.appBar} position="static" color="inherit">
 					<Typography className={classes.heading} variant="h2">
 						Memories{" "}
 					</Typography>
